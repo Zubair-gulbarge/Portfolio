27202e5e0f3cf31ec1375180a5a5457bc6b97d42
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-08-27 03:21:55 +0530

    Updated second.py

--- python/second.py
+++ python/second.py
@@ -1,3 +1,21 @@
 # Hello World:
 
-print("Hello, World!")
+# print("Hello, World!")
+
+# Addition of Two Numbers:
+
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# sum = num1 + num2
+# print("Sum:", sum)
+
+# Factorial Calculation:
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial(num))
